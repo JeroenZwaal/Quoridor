c9363e290e44c797dafd1c9418c3c705ddb4487c
Hugo Zwaal <hugo.zwaal@mendix.com>
2023-12-30 15:22:31 +0100

    Add exception and rejection handler to route

--- src/main/scala/nl/zwaaltjes/quoridor/server/Router.scala
+++ src/main/scala/nl/zwaaltjes/quoridor/server/Router.scala
@@ -4,8 +4,9 @@ import org.apache.pekko.actor.typed.scaladsl.AskPattern.*
 import org.apache.pekko.actor.typed.{ActorRef, ActorSystem}
 import org.apache.pekko.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
 import org.apache.pekko.http.scaladsl.model.{StatusCodes, Uri}
-import org.apache.pekko.http.scaladsl.model.headers.{BasicHttpCredentials, HttpChallenges, Location, RawHeader}
+import org.apache.pekko.http.scaladsl.model.headers.*
 import org.apache.pekko.http.scaladsl.server.*
+import org.apache.pekko.http.scaladsl.server.AuthenticationFailedRejection.{CredentialsMissing, CredentialsRejected}
 import org.apache.pekko.http.scaladsl.server.Directives.*
 import org.apache.pekko.http.scaladsl.server.directives.Credentials
 import org.apache.pekko.util.Timeout
@@ -13,20 +14,66 @@ import spray.json.DefaultJsonProtocol.*
 
 import scala.concurrent.duration.DurationInt
 
+// TODO: game protocol
+// TODO: content negotiation
 object Router {
   private val SessionCookie = "QuoridorSession"
   private val SessionHeader = "XQuoridorSession"
   private val Realm = "quoridor"
 
-  private val InvalidCredentials =
-    AuthenticationFailedRejection(AuthenticationFailedRejection.CredentialsRejected, HttpChallenges.basic(Realm))
+  private val InvalidCredentials = AuthenticationFailedRejection(CredentialsRejected, HttpChallenges.basic(Realm))
+  private val MissingCredentials = AuthenticationFailedRejection(CredentialsMissing, HttpChallenges.basic(Realm))
 
-  private val MissingCredentials =
-    AuthenticationFailedRejection(AuthenticationFailedRejection.CredentialsMissing, HttpChallenges.basic(Realm))
-
-  private val authenticator: Credentials => Option[String] = {
+  private val adminAuthenticator: Credentials => Option[String] = {
     case p @ Credentials.Provided("wortel") if p.verify("p4$sw0rd") => Some(p.identifier)
     case _ => None
+  }
+
+  private val rejectionHandler: RejectionHandler = RejectionHandler
+    .newBuilder()
+    .handleAll[MethodRejection] { rejections =>
+      val (methods, names) = rejections.map(r => r.supported -> r.supported.name).unzip
+      val message = "HTTP method not allowed, supported methods: " + names.mkString(", ")
+      complete(StatusCodes.MethodNotAllowed, Seq(Allow(methods)), Json.Error(message))
+    }
+    .handle {
+      case MalformedHeaderRejection(header, message, _) =>
+        complete(StatusCodes.BadRequest, s"The value of HTTP header '$header' was malformed:\n$message")
+    }
+    .handle {
+      case RequestEntityExpectedRejection =>
+        complete(StatusCodes.BadRequest, "Request entity expected but not supplied.")
+    }
+    .handleAll[UnsupportedRequestContentTypeRejection] { rejections =>
+      val unsupported = rejections.find(_.contentType.isDefined).flatMap(_.contentType).fold("")(" [" + _ + "]")
+      val supported = rejections.flatMap(_.supported).mkString(" or ")
+      val expected = if (supported.isEmpty) "" else s" Expected:\n$supported"
+      complete(StatusCodes.UnsupportedMediaType, s"The request's Content-Type$unsupported is not supported.$expected")
+    }
+    .handleAll[UnacceptedResponseContentTypeRejection] { rejections =>
+      val supported = rejections.flatMap(_.supported).map(_.format)
+      val message = supported.mkString("Resource representation is only available with these types:\n", "\n", "")
+      complete(StatusCodes.NotAcceptable, message)
+    }
+    .handle {
+      case AuthenticationFailedRejection(cause, challenge) =>
+        val message = cause match {
+          case CredentialsMissing => "The resource requires authentication, which was not supplied with the request."
+          case CredentialsRejected => "The supplied authentication is invalid."
+        }
+        complete(StatusCodes.Unauthorized, Seq(`WWW-Authenticate`(challenge)), Json.Error(message))
+    }
+    .handle {
+      case ValidationRejection(message, _) =>
+        complete(StatusCodes.BadRequest, Json.Error(message))
+    }
+    .handle { case rejection => sys.error(s"Unhandled rejection: $rejection") }
+    .handleNotFound { complete(StatusCodes.NotFound, Json.Error("The requested resource could not be found.")) }
+    .result()
+
+  private val exceptionHandler: ExceptionHandler = ExceptionHandler {
+    case e: Throwable =>
+      complete(StatusCodes.InternalServerError, Json.Error(s"Something went terribly wrong.\n\n$e"))
   }
 
   private def setSessionHeader(sessionId: SessionId): Directive0 =
@@ -45,128 +92,138 @@ class Router(
   given ActorSystem[HttpServer.Command] = httpServer
 
   def route(base: Uri): Route =
-    concat(
-      pathEndOrSingleSlash {
-        get {
-          getFromResource("www/welcome.html")
-        }
-      },
-      path("version") {
-        get {
-          onSuccess(httpServer.ask(HttpServer.GetVersion.apply)) {
-            case HttpServer.Version(version) =>
-              complete(StatusCodes.OK, Json.OK(version))
-          }
-        }
-      },
-      path("shutdown") {
-        post {
-          authenticateBasic(realm = Realm, authenticator) { _ =>
-            httpServer ! HttpServer.Shutdown
-            complete(StatusCodes.Accepted, Json.OK.empty)
-          }
-        }
-      },
-      pathPrefix("users") {
+    handleExceptions(exceptionHandler) {
+      handleRejections(rejectionHandler) {
         concat(
-          pathPrefix(UserId.Segment) { userId =>
-            pathEnd {
-              concat(
-                get {
-                  authenticatedUser { _ =>
-                    userRequest(UserServer.GetUser(userId, _)) {
-                      case UserServer.UserData(_, name, email) =>
-                        complete(StatusCodes.OK, Json.OK(Json.UserData(userId = userId, name = name, email = email)))
-                      case UserServer.UnknownUser(message) =>
-                        complete(StatusCodes.NotFound, Json.Error(message))
-                    }
-                  }
-                },
-                put {
-                  optionalAuthenticatedUser { authenticatedUserId =>
-                    entity(as[Json.UserDetails]) { user =>
-                      userRequest(UserServer.UpdateUser(userId, user.email, user.name, user.password, authenticatedUserId, _)) {
-                        case ok @ UserServer.OK(Some(sessionId)) =>
-                          setSessionHeader(sessionId) {
-                            val uri = Uri(s"users/$userId").resolvedAgainst(base)
-                            complete(StatusCodes.Created, Seq(Location(uri)), Json.OK.empty)
-                          }
-                        case ok @ UserServer.OK(None) =>
-                          complete(StatusCodes.OK, Json.OK.empty)
-                        case UserServer.UserExists(message) =>
-                          complete(StatusCodes.BadRequest, Json.Error(message))
-                        case UserServer.UnknownUser(message) =>
-                          complete(StatusCodes.NotFound, Json.Error(message))
-                        case UserServer.WrongUser(message) =>
-                          complete(StatusCodes.Forbidden, Json.Error(message))
-                      }
-                    }
-                  }
-                },
-              )
+          pathEndOrSingleSlash {
+            get {
+              getFromResource("www/welcome.html")
             }
           },
-          pathEnd {
+          path("version") {
             get {
-              userRequest(UserServer.GetDump.apply) {
-                case UserServer.Dump(data) =>
-                  complete(StatusCodes.OK, data)
+              onSuccess(httpServer.ask(HttpServer.GetVersion.apply)) {
+                case HttpServer.Version(version) =>
+                  complete(StatusCodes.OK, Json.OK(version))
               }
             }
+          },
+          path("shutdown") {
+            post {
+              authenticateBasic(realm = Realm, adminAuthenticator) { _ =>
+                httpServer ! HttpServer.Shutdown
+                complete(StatusCodes.Accepted, Json.OK.empty)
+              }
+            }
+          },
+          pathPrefix("users") {
+            userRoute(Uri("users").resolvedAgainst(base))
+          },
+          pathPrefix("games") {
+            gameRoute(Uri("games").resolvedAgainst(base))
           },
         )
-      },
-      pathPrefix("games") {
-        concat(
-          pathEnd {
+      }
+    }
+
+  private def userRoute(base: Uri): Route =
+    concat(
+      pathPrefix(UserId.Segment) { userId =>
+        pathEnd {
+          concat(
             get {
-              authenticatedUser { userId =>
-                complete(StatusCodes.NotImplemented) // TODO
+              authenticatedUser { _ =>
+                userRequest(UserServer.GetUser(userId, _)) {
+                  case UserServer.UserData(_, name, email) =>
+                    complete(StatusCodes.OK, Json.OK(Json.UserData(userId = userId, name = name, email = email)))
+                  case UserServer.UnknownUser(message) =>
+                    complete(StatusCodes.NotFound, Json.Error(message))
+                }
               }
-            }
-          },
-          pathPrefix("invite") {
-            path(Segment) { invitee =>
-              post {
-                authenticatedUser { userId =>
-                  gameRequest(GameServer.NewGame(Seq(userId, UserId(invitee)), _)) {
-                    case GameServer.Created(gameId) =>
-                      val uri = Uri(s"games/$gameId").resolvedAgainst(base)
-                      complete(StatusCodes.Created, Seq(Location(uri)), Json.OK.empty)
-                    case GameServer.Error(message) =>
+            },
+            put {
+              optionalAuthenticatedUser { authenticatedUserId =>
+                entity(as[Json.UserDetails]) { user =>
+                  userRequest(UserServer.UpdateUser(userId, user.email, user.name, user.password, authenticatedUserId, _)) {
+                    case ok @ UserServer.OK(Some(sessionId)) =>
+                      setSessionHeader(sessionId) {
+                        val uri = Uri(s"$userId").resolvedAgainst(base)
+                        complete(StatusCodes.Created, Seq(Location(uri)), Json.OK.empty)
+                      }
+                    case ok @ UserServer.OK(None) =>
+                      complete(StatusCodes.OK, Json.OK.empty)
+                    case UserServer.UserExists(message) =>
                       complete(StatusCodes.BadRequest, Json.Error(message))
+                    case UserServer.UnknownUser(message) =>
+                      complete(StatusCodes.NotFound, Json.Error(message))
+                    case UserServer.WrongUser(message) =>
+                      complete(StatusCodes.Forbidden, Json.Error(message))
                   }
                 }
               }
-            }
-          },
-          pathPrefix(GameId.Segment) { gameId =>
+            },
+          )
+        }
+      },
+      pathEnd {
+        get {
+          userRequest(UserServer.GetDump.apply) {
+            case UserServer.Dump(data) =>
+              complete(StatusCodes.OK, data)
+          }
+        }
+      },
+    )
+
+  private def gameRoute(base: Uri): Route =
+    concat(
+      pathEnd {
+        get {
+          authenticatedUser { userId =>
+            complete(StatusCodes.NotImplemented) // TODO
+          }
+        }
+      },
+      pathPrefix("invite") {
+        path(Segment) { invitee =>
+          post {
             authenticatedUser { userId =>
-              concat(
-                pathEnd {
-                  get {
-                    complete(StatusCodes.NotImplemented) // TODO
-                  }
-                },
-                path("accept") {
-                  post {
-                    complete(StatusCodes.NotImplemented) // TODO
-                  }
-                },
-                path("reject") {
-                  post {
-                    complete(StatusCodes.NotImplemented) // TODO
-                  }
-                },
-                path("move") {
-                  post {
-                    complete(StatusCodes.NotImplemented) // TODO
-                  }
-                },
-              )
+              gameRequest(GameServer.NewGame(Seq(userId, UserId(invitee)), _)) {
+                case GameServer.Created(gameId) =>
+                  val uri = Uri(s"$gameId").resolvedAgainst(base)
+                  complete(StatusCodes.Created, Seq(Location(uri)), Json.OK.empty)
+                case GameServer.Error(message) =>
+                  complete(StatusCodes.BadRequest, Json.Error(message))
+              }
             }
-          },
-        )
+          }
+        }
+      },
+      pathPrefix(GameId.Segment) { gameId =>
+        authenticatedUser { userId =>
+          concat(
+            pathEnd {
+              get {
+                complete(StatusCodes.NotImplemented) // TODO
+              }
+            },
+            path("accept") {
+              post {
+                complete(StatusCodes.NotImplemented) // TODO
+              }
+            },
+            path("reject") {
+              post {
+                complete(StatusCodes.NotImplemented) // TODO
+              }
+            },
+            path("move") {
+              post {
+                complete(StatusCodes.NotImplemented) // TODO
+              }
+            },
+          )
+        }
       },
     )
 
@@ -176,39 +233,34 @@ class Router(
   private def gameRequest[A](create: ActorRef[A] => GameServer.Command): Directive1[A] =
     onSuccess(gameServer.ask(create))
 
-  private def optionalAuthenticatedUser: Directive1[Option[UserId]] =
+  private def authenticatedUser: Directive1[UserId] =
     sessionHeader | sessionCookie | basicAuthentication
 
-  private def authenticatedUser: Directive1[UserId] =
-    optionalAuthenticatedUser.flatMap {
-      case Some(userId) =>
-        provide(userId)
+  private def optionalAuthenticatedUser: Directive1[Option[UserId]] =
+    (authenticatedUser & cancelRejection(MissingCredentials)).map(Option.apply) | provide(None)
+
+  private val sessionHeader: Directive1[UserId] =
+    optionalHeaderValueByName(SessionHeader).flatMap {
+      case Some(session) =>
+        validate(SessionId(session))
       case None =>
         reject(MissingCredentials)
     }
 
-  private val sessionHeader: Directive1[Option[UserId]] =
-    optionalHeaderValueByName(SessionHeader).flatMap {
-      case Some(session) =>
-        validate(SessionId(session)).map(Option.apply)
-      case None =>
-        provide(None)
-    }
-
-  private val sessionCookie: Directive1[Option[UserId]] =
+  private val sessionCookie: Directive1[UserId] =
     optionalCookie(SessionCookie).flatMap {
       case Some(cookie) =>
-        validate(SessionId(cookie.value)).map(Option.apply)
+        validate(SessionId(cookie.value))
       case None =>
-        provide(None)
+        reject(MissingCredentials)
     }
 
-  private val basicAuthentication: Directive1[Option[UserId]] =
+  private val basicAuthentication: Directive1[UserId] =
     extractCredentials.flatMap {
       case Some(BasicHttpCredentials(userId, password)) =>
-        authenticate(UserId(userId), Password(password)).map(Option.apply)
+        authenticate(UserId(userId), Password(password))
       case _ =>
-        provide(None)
+        reject(MissingCredentials)
     }
 
   private def validate(sessionId: SessionId): Directive1[UserId] =
